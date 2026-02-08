5d46506901e3ccb7151b81b61a1e87cfad1d5d1b
Leif <kruseleif@gmail.com>
2026-02-08 18:17:43 +0100

    prototype frontend done, can draw points and polygon

--- frontend/Zeitkarte_Frankfurt_am_Main_ng_app/src/app/map-view/map-view.ts
+++ frontend/Zeitkarte_Frankfurt_am_Main_ng_app/src/app/map-view/map-view.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { MapComponent } from '@maplibre/ngx-maplibre-gl';
+import { Component, effect, inject } from '@angular/core';
+import { MapComponent, GeoJSONSourceComponent, FeatureComponent, LayerComponent } from '@maplibre/ngx-maplibre-gl';
+import { MapService } from './map.service';
 
 @Component({
-  selector: 'app-map-view',
-  imports: [MapComponent],
-  templateUrl: './map-view.html',
-  styleUrl: './map-view.scss',
+    selector: 'app-map-view',
+    imports: [MapComponent, GeoJSONSourceComponent, FeatureComponent, LayerComponent,],
+    templateUrl: './map-view.html',
+    styleUrl: './map-view.scss',
 })
 export class MapView {
+    mapService = inject(MapService);
+    geometry = this.mapService.geoJson;
+    multiPolygon = this.mapService.blobJson;
 
+
+    constructor() {
+        effect(() => {
+            console.log("Data arrived:", this.geometry());
+            console.log("multiPolygon", this.multiPolygon());
+        });
+    }
 }
---
+++ frontend/Zeitkarte_Frankfurt_am_Main_ng_app/src/app/map-view/map.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IsochroneGeoJson } from '../models/geo-data.model';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class MapService {
+    private http = inject(HttpClient);
+
+    private geoJson$ = this.http.get<IsochroneGeoJson>('http://localhost:5003/api/Isochrones');
+    geoJson = toSignal(this.geoJson$);
+
+    private blobJson$ = this.http.get<string>('http://localhost:5003/api/Isochrones/Blob');
+    blobJson = toSignal(this.blobJson$);
+}
