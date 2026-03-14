627e07e2e048fda33fe95857c89c6f1de9a2fc70
Leif <kruseleif@gmail.com>
2026-03-14 12:45:21 +0100

    Add environment.ts with placeholder

--- frontend/Zeitkarte_Frankfurt_am_Main_ng_app/build.sh
+++ frontend/Zeitkarte_Frankfurt_am_Main_ng_app/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
-
-# Replace placeholder with the real key from Cloudflare's environment
-sed -i "s|API_KEY_PLACEHOLDER|$API_KEY|g" src/environments/environment.prod.ts
-
-# Build the app
+sed -i "s|API_KEY_PLACEHOLDER|$API_KEY|g" src/environments/environment.ts
 ng build --configuration production
